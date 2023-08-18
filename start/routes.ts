50474fa83b8ccac9c3a36076cd4e825d185868ba
Dan Romano <dan@anrosol.com>
2023-08-18 19:03:10 -0400

    Added routes.

--- start/routes.ts
+++ start/routes.ts
@@ -19,7 +19,53 @@
 */
 
 import Route from '@ioc:Adonis/Core/Route'
+import Post from 'App/Models/Post'
+import { marked } from 'marked';
+
+// ********************************************************************************
+// Be sure to look at `requirements.md` to undestand what this application should do.
+// ********************************************************************************
 
 Route.get('/', async ({ view }) => {
-  return view.render('welcome')
-})
+    const posts = await Post.query()
+        .where('public', '=', true)
+        .orderBy('createdAt', 'desc')
+        .preload('user')
+    
+    const lastest_post = posts[0]
+    const [, ...older_posts] = posts
+
+    return view.render('index', {
+        markdown: marked,
+        latest_post: lastest_post,
+        older_posts: older_posts,
+    })
+}).as('root')
+
+Route.get('/sign-up', 'AuthController.signUp').as('auth.sign-up')
+
+Route.post('/sign-up', 'AuthController.signUpPost').as('auth.sign-up.post')
+
+Route.get('/login', 'AuthController.login').as('auth.login')
+
+Route.post('/login', 'AuthController.loginPost').as('auth.login.post')
+
+Route.group(() => {
+    Route.post('/logout', 'AuthController.logout').as('auth.logout')
+
+    Route.get('/dashboard', 'AuthController.dashboard').as('auth.dashboard')
+
+    Route.get('/profile', 'AuthController.profile').as('auth.profile')
+
+    Route.post('/profile', 'AuthController.profilePost').as('auth.profile.post')
+
+    Route.resource('/users', 'UsersController').except(['show']).as('users')
+
+    Route.resource('/posts', 'PostsController').except(['show']).as('posts')
+
+    Route.resource('/comments', 'CommentsController').except(['store']).as('comments')
+}).middleware(['auth', 'active'])
+
+Route.resource('/users', 'UsersController').only(['show']).as('users')
+Route.resource('/posts', 'PostsController').only(['show']).as('posts')
+Route.resource('/comments', 'CommentsController').only(['store']).as('comments')
